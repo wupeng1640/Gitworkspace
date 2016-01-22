d6d2d753c8328b7aa6349bcbe872e366b3b5f6dd
wupeng1640 <164553538@qq.com>
2016-01-22 16:19:33 +0800

    idea 修改工程

--- com.MyWorkSpace.Java/src/Main.java
+++ com.MyWorkSpace.Java/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("sdjaskda就开xzxx始的萨克sadsadsa达斯柯达看萨从自行车行啊飒飒的克达SD卡撒");
+        System.out.println("Hello World!，你好吗！！！");
     }
 }
