fc331ee1a1fb685a81e6af49f458cfe12afd4b9f
wupeng1640 <164553538@qq.com>
2016-01-22 15:00:39 +0800

    2016.1.22 15:30  version 1.0.1

---
+++ com.MyWorkSpace.Java/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("sdjaskda就开xzxx始的萨克达斯柯达看萨从自行车行啊飒飒的克达SD卡撒");
+    }
+}
