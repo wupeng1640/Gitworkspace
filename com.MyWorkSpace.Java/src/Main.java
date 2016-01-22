1310c2fe3021a9886a093080774ac17dec643c2f
wupeng1640 <164553538@qq.com>
2016-01-22 16:38:16 +0800

    e盘内提交内容

--- com.MyWorkSpace.Java/src/Main.java
+++ com.MyWorkSpace.Java/src/Main.java
@@ -1,8 +1,14 @@
+import org.apache.xpath.SourceTree;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("sdjaskda就开xzxx始的萨克sadsadsa达斯柯达看萨从自行车行啊飒飒的克达SD卡撒");
-        System.out.println("Hello World!，你好吗！！！");
+        System.out.println("Hello World!，你好吗的撒旦！！！");
+        int i=100;
+        System.out.println("dasjdasjd "+i);
+
+
     }
 }
